dcab8c61908cc5b189872a53fecc7687ba36d182
Harmeetkaur Sohi <harmeet.kaur@b2datastream.com>
2023-06-21 14:01:59 +0530

    paging params updates

--- src/main/kotlin/dev/tmsoft/lib/query/paging/PagingParameters.kt
+++ src/main/kotlin/dev/tmsoft/lib/query/paging/PagingParameters.kt
@@ -17,7 +17,7 @@ fun Parameters.paging(maxPageSize: Int = 100, default: Int = 30): PagingParamete
     )
 }
 
-data class TestParameters(val pageSize: Int, val currentPage: Int, val searchText : String) {
+data class TestParams(val pageSize: Int, val currentPage: Int, val searchText : String) {
     val offset = offset(pageSize, currentPage)
 }
 data class PagingParameters(val pageSize: Int, val currentPage: Int) {
